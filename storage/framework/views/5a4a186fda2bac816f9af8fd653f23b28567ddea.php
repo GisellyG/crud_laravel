dd6677637a0384ed0773ef2314ab46559e13eb02
GisellyG <gisellygondim@gmail.com>
2024-10-12 17:16:41 -0300

    Add files via upload

---
+++ storage/framework/views/5a4a186fda2bac816f9af8fd653f23b28567ddea.php
@@ -0,0 +1,9 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+<h2>Home</h2>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\gisel\Documents\Laravel\CRUD1\resources\views/home.blade.php ENDPATH**/ ?>
